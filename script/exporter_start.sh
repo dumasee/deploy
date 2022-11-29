487b82017478257506f741923f335e650f4c6de7
dumasee <phoebus999@126.com>
2022-11-29 15:54:49 +0800

    update

--- script/exporter_start.sh
+++ script/exporter_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #     
-#     exporter_start.sh @ Version 0.17
+#     exporter_start.sh @ Version 0.19
 #     date: 2022.2.24
 
 ps -ef|grep node_exporter |grep -qv grep && echo prometheus-node is running. && exit 0
 mkdir -p /tmp/textcollector
-cd /opt/node_exporter*/ && nohup ./node_exporter --collector.textfile.directory="/tmp/textcollector" > /dev/null 2>&1 &
+cd /opt/node_exporter*/ && nohup ./node_exporter --web.listen-address=":9100" --collector.textfile.directory="/tmp/textcollector" > /dev/null 2>&1 &
 
